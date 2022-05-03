365f74173821392b8c597d75ede96ca8c3bb37ca
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-05-03 22:38:19 +0300

    Use RSpec test and Rubocop

---
+++ spec/lib/exchange_it/user_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe ExchangeIt::User do
+  let(:user) { described_class.new 'John', 'Doe' }
+  let(:user_no_name) { described_class.new nil, 'Doe' }
+
+  it 'returns name' do
+    expect(user.name).to eq('John')
+    expect(user.name).to be_an_instance_of(String)
+  end
+
+  it 'returns name as a string' do
+    user = described_class.new nil, 'Doe'
+    expect(user_no_name.name).to be_an_instance_of(String)
+  end
+
+  it 'returns surname' do
+    expect(user.surname).to eq('Doe')
+  end
+end
